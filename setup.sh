b62db26c93b077585f0081b3564db4412d2663e1
Simon Tate <simontate94@gmail.com>
2019-11-04 10:21:15 +0000

    Remove bad whitespace

--- setup.sh
+++ setup.sh
@@ -3,10 +3,10 @@
 [[ ! -e bashrc ]] && echo "Must be ran in dotfiles directory." && exit 255;
 [[ -z $HOME ]] && echo "HOME must be set." && exit 255;
 
-function usage { 
+function usage {
     echo "Valid options:"
     echo "      -h|--help ........ display this text"
-    echo "      -f|--force ....... forces copy of dotfiles - even if they exist"        
+    echo "      -f|--force ....... forces copy of dotfiles - even if they exist"
 }
 
 
@@ -33,7 +33,7 @@ for f in $(find . -maxdepth 1 -type f ! -samefile ${0}); do
         ln -sf "$PWD/$f" "$HOME/.$f"
     else
         echo "$HOME/.$f already exists."
-    fi     
+    fi
 done
 
 exit
